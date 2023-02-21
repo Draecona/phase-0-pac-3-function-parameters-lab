61849c96c9fc874da432719809d3e5419c699f28
Brandi Hanks <brandihanks85@gmail.com>
2023-02-20 20:56:03 -0600

    complete lab

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction(name) {
+    return "Hi, my name is Draecona.";
+}
+
+function introductionWithLanguage(name, language) {
+    return "Hi, my name is Draecona and I am learning to program in JavaScript.";
+}
+
+function introductionWithLanguageOptional(name, language) {
+    return "Hi, my name is Draecona and I am learning to program in JavaScript."; 
+}
